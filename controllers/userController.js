cf39fa5ab9c6047e708f7742c11702788449940e
Luís Felipe <luisf.csdev@gmail.com>
2022-10-05 01:02:31 -0300

    token added

--- controllers/userController.js
+++ controllers/userController.js
@@ -109,8 +109,48 @@
 
 
 //LOGIN WITH USER
+// const User = require('../models/User')
+// const bcrypt = require('bcryptjs');
+
+// const userController = {
+
+//     register: async function register(req, res) {
+//         const selectedUser = await User.findOne({email: req.body.email});
+//         if (selectedUser) return res.status(400).send('Email already exists');
+
+//         const user = new User({
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: bcrypt.hashSync(req.body.password)
+//         })
+
+//         try{
+//             const savedUser = await user.save();
+//             res.send(savedUser);
+//         } catch (error) {
+//             res.status(400).send(error);
+//         }
+//     },
+
+//     login: async function login(req, res) {
+//         const selectedUser = await User.findOne({email: req.body.email});
+//         if (!selectedUser) return res.status(400).send('Email or password incorrect');
+    
+//         const passwordMatch = bcrypt.compareSync(req.body.password, selectedUser.password)
+//         if(!passwordMatch) return res.status(400).send('Email or password incorrect');
+
+//         res.send('User Logged');
+//     }
+
+// }
+
+// module.exports = userController
+
+
+//CREATING TOKEN
 const User = require('../models/User')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userController = {
 
@@ -139,6 +179,9 @@ const userController = {
         const passwordMatch = bcrypt.compareSync(req.body.password, selectedUser.password)
         if(!passwordMatch) return res.status(400).send('Email or password incorrect');
 
+        const token = jwt.sign({_id: selectedUser._id}, process.env.TOKEN_SECRET);
+
+        res.header('authorization-token', token);
         res.send('User Logged');
     }
 
